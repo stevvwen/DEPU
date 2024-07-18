613d64c0f0ff25fcebcb023dff4e521d93f5afc4
stevvwen <shuo.wen@mail.mcgill.ca>
2024-07-17 20:59:39 -0700

    Update the training

--- test_td.sh
+++ test_td.sh
@@ -1,1 +1,13 @@
+#!/bin/bash
+#SBATCH --time=20:00:00
+#SBATCH --account=rrg-bengioy-ad
+#SBATCH --output=scripts/out2.txt
+#SBATCH --gres=gpu:v100l:1
+#SBATCH --mem=64000M
+
+module load python/3.10
+module load cuda
+
+source ../depu/bin/activate
+
 python test_td3_training.py
