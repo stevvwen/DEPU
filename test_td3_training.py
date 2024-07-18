07103cd110b3b5923123e2494bc5a836c3464624
SediRockStar <shuo.wen@mail.mcgill.ca>
2024-07-18 12:01:49 +0800

    Add wandb init to the project

--- test_td3_training.py
+++ test_td3_training.py
@@ -1,11 +1,10 @@
-from agents.td3 import *
-import hydra
-from omegaconf import DictConfig
-from core.runner.runner import *
+from datetime import datetime
+import wandb
 from core.data.agent_trainer import AgentTrainer
-
-#tmp
+from core.runner.runner import *
+# tmp
 from core.utils.utils import make_env, make_agent
+
 
 @hydra.main(config_path="configs", config_name="base", version_base='1.2')
 def training_td3_for_data(config: DictConfig):
@@ -13,6 +12,15 @@ def training_td3_for_data(config: DictConfig):
     env, obs_dim, act_dim = make_env(config.rl_env)
     agent= make_agent(obs_dim, act_dim, config.agent.agent)
     replay_buffer= hydra.utils.instantiate(config.replay_buffer)
+    wandb.login()
+
+    run = wandb.init(
+        # Set the project where this run will be logged
+        project="DEPU",
+        name= datetime.now().strftime('%y%m%d_%H%M%S'),
+        config=config.agent.agent,
+        allow_val_change=True
+    )
     trainer= AgentTrainer(config, env= env, model= agent, replay_buffer= replay_buffer)
     trainer.rollout()
 
