51cfae382667dc62837637c64b4a40fa44ca0267
stevvwen <shuo.wen@mail.mcgill.ca>
2024-07-21 19:35:25 -0700

    td3 config change

---
+++ sb.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --time=20:00:00
+#SBATCH --account=rrg-bengioy-ad
+#SBATCH --output=scripts/out3.txt
+#SBATCH --gres=gpu:v100l:1
+#SBATCH --mem=32000M
+
+module load python/3.11
+
+source ../depu/bin/activate
+
+python train.py --algo td3 --env Pendulum-v1 --n-timesteps 1000000 --log-interval 10 --eval-freq 1000
+python scripts/plot_train.py -a td3 -e Pendulum-v1 -y success -f rl-trained-agents/ -w 500 -x steps
