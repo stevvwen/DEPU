a2fd36f3b9306e0dd526c7afdbaa1335be039e11
SediRockStar <shuo.wen@mail.mcgill.ca>
2024-07-22 16:07:09 +0800

    Evaluation function

--- core/data/agent_trainer.py
+++ core/data/agent_trainer.py
@@ -1,10 +1,8 @@
-from collections import deque
-
 import wandb
 
 
 class AgentTrainer:
-    def __init__(self, config, env, model, replay_buffer, eval_mode= False):
+    def __init__(self, config, env, model, replay_buffer, eval_episode_freq):
         self.env = env
         self.reward_trace = 0  # trace of reward
 
@@ -18,9 +16,14 @@ class AgentTrainer:
         self.debug_mode = config.debug_mode
         self.buffer = replay_buffer
         self.model= model
-        self.eval_mode= eval_mode
+        self.eval_epi_freq= eval_episode_freq
+        self.eval_mode= False
 
         self.cum_reward= 0
+
+        self.avg_epi_reward= 0
+
+        self.eval_count= 0
 
     def rollout(self):
 
@@ -31,9 +34,7 @@ class AgentTrainer:
         # Rollout a trajectory with 16 samples
         for step in range(self.max_steps):
 
-
-
-            action = self.model.act(state, step, self.eval_mode)  # Select action
+            action = self.model.act(state, step, False)  # Select action
             next_state, reward, terminated, truncated, info = self.env.step(action)  # Step
             self.epi_steps += 1
 
@@ -60,6 +61,12 @@ class AgentTrainer:
                 self.epi_count += 1
                 self.epi_reward = 0
                 self.epi_steps = 0
+
+                if self.epi_count % self.eval_epi_freq == 0:
+                    self.eval_mode = True
+                    self.evaluate()
+                    self.eval_mode = False
+
                 state, _ = self.env.reset()
 
             self.cur_state = state
@@ -73,3 +80,32 @@ class AgentTrainer:
 
         print("Single agent training complete")
         return
+
+
+    def evaluate(self):
+        print(f"Agent evaluation episode {self.epi_count/self.eval_epi_freq} starts")
+
+        state, _ = self.env.reset()
+
+        terminated= False
+        truncated= False
+
+        eval_rewards= 0
+        eval_epi_steps= 0
+
+        while not (terminated or truncated):
+            action = self.model.act(state, self.model.num_expl_steps, self.eval_mode)  # Select action
+            next_state, reward, terminated, truncated, info = self.env.step(action)  # Step
+
+            eval_rewards+= reward
+
+            eval_epi_steps+= 1
+
+        if not self.debug_mode:
+            wandb.log({"Eval Epi Reward": eval_rewards, "eval_epi_count": self.eval_epi_freq})
+
+        print(f"Agent evaluation episode {self.epi_count/self.eval_epi_freq} "
+              f"with Episode Reward {eval_rewards} and Episode Length {eval_epi_steps}")
+
+        return eval_rewards, eval_epi_steps
+
