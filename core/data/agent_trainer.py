61d707cbc42e08a0de1246b32a94413dd201c07a
SediRockStar <shuo.wen@mail.mcgill.ca>
2024-07-27 12:33:13 +0800

    Reconstruct the evaluation process

--- core/data/agent_trainer.py
+++ core/data/agent_trainer.py
@@ -17,7 +17,7 @@ class AgentTrainer:
         self.debug_mode = config.debug_mode
         self.buffer = replay_buffer
         self.model= model
-        self.eval_epi_freq= config.eval_episode_freq
+        self.eval_eval_interval= config.eval_eval_interval
         self.eval_mode= False
 
         self.cum_reward= 0
@@ -39,7 +39,8 @@ class AgentTrainer:
             next_state, reward, terminated, truncated, info = self.env.step(action)  # Step
             self.epi_steps += 1
 
-            # reward= 16*reward # normalize reward
+            reward= (reward+ 8)/ 8 #TODO: Change this to a more general form
+
             # If the episode is truncated, we still do the update
             mask = 1 if truncated else float(not terminated)
 
@@ -63,10 +64,10 @@ class AgentTrainer:
                 self.epi_reward = 0
                 self.epi_steps = 0
 
-                if self.epi_count % self.eval_epi_freq == 0:
-                    self.eval_mode = True
-                    self.evaluate()
-                    self.eval_mode = False
+            if step % self.eval_eval_interval == 0:
+                self.eval_mode = True
+                self.evaluate()
+                self.eval_mode = False
 
                 state, _ = self.env.reset()
 
@@ -83,33 +84,36 @@ class AgentTrainer:
         return
 
 
-    def evaluate(self):
-        print(f"Agent evaluation episode {self.epi_count/self.eval_epi_freq} starts")
+    def evaluate(self, turns= 3):
 
-        state, _ = self.env.reset()
 
-        terminated= False
-        truncated= False
+        total_score= 0
+        eval_epi_steps = 0
+        for _ in range(turns):
+            state, _ = self.env.reset()
 
-        eval_rewards= 0
-        eval_epi_steps= 0
+            done= False
 
-        while not (terminated or truncated):
-            with torch.no_grad():
-                action = self.model.act(state, self.model.num_expl_steps, False)  # Select action
-            next_state, reward, terminated, truncated, info = self.env.step(action)  # Step
+            while not done:
+                with torch.no_grad():
+                    action = self.model.act(state, self.model.num_expl_steps, False)  # Select action
 
-            eval_rewards+= reward
+                next_state, reward, terminated, truncated, info = self.env.step(action)  # Step
+                done= terminated or truncated
+                total_score+= reward
 
-            eval_epi_steps+= 1
+                eval_epi_steps+= 1
+                state= next_state
 
-            state= next_state
+        avg_score= total_score/turns
+        avg_step= eval_epi_steps/turns
+
+        self.eval_count+= 1
 
         if not self.debug_mode:
-            wandb.log({"Eval Epi Reward": eval_rewards, "eval_epi_count": self.epi_count/self.eval_epi_freq})
+            wandb.log({"Eval Reward": avg_score, "eval_epi_count": self.eval_count})
 
-        print(f"Agent evaluation episode {self.epi_count/self.eval_epi_freq} "
-              f"with Episode Reward {eval_rewards} and Episode Length {eval_epi_steps}")
+        print(f"Agent evaluation turn {self.eval_count} "
+              f"with Episode Reward {avg_score} and Episode Length {avg_step}")
 
-        return eval_rewards, eval_epi_steps
-
+        return avg_score, eval_epi_steps
